54c24fba16adfb74580cce889489799d65071f4f
Guilherme Valdevieso <gsvaldevieso@gmail.com>
2017-07-18 20:03:16 -0300

    estrutura inicial do projeto

---
+++ database/migrations/2017_07_18_224818_create_movement_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMovementTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movements', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('description');
+            $table->decimal('value', 6, 2);
+            $table->char('type', 2);
+            $table->date('date');
+            $table->integer('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('movements');
+    }
+}
