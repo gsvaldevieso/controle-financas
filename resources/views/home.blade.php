50debd467f6cab5d266fb85cb6809d306306f35f
Guilherme Valdevieso <gsvaldevieso@gmail.com>
2017-07-18 20:21:06 -0300

    criacao da estrutura de views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,25 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Página inicial</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    <h4>Suas contas</h4>
+                    <hr>
+                    <a class="btn btn-primary">
+                        <b>Número: 13123123-123</b>
+                        <br>
+                        <b>Banco: Itaú</b>
+                        <br>
+                        Titular: Guilherme Soares Valdevieso
+                        <br>
+                        Saldo: R$ 10,00
+                    </a>
+                    <a class="btn btn-success">
+                        <h1>+</h1>
+                    </a>
                 </div>
+
             </div>
         </div>
     </div>
