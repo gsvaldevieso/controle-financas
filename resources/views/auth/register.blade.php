34aec910ee0a6a15f1759ddf5adab2328189a74b
Guilherme Valdevieso <gsvaldevieso@gmail.com>
2017-08-05 14:26:40 -0300

    adicao da tabela de cidades e estados e alteracao em algumas escritas

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Criar nova conta</div>
+                <div class="panel-heading">Junte-se a nós</div>
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
@@ -63,7 +63,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Cadastrar-me
+                                    Cadastrar e começar a economizar!
                                 </button>
                             </div>
                         </div>
